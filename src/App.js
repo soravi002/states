28bbf5d0245d73abe96b6b82a14e9852e6c443c8
nameless <gomisezekielpaulantoine@gmail.com>
2024-01-17 13:46:54 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      person: {
+        fullName: 'Ezékiel Goméz',
+        bio: 'Je sais pas quoi écrire.',
+        imgSrc: 'https://i.pinimg.com/564x/3f/87/44/3f8744a0639e336983a389a41d59a7b2.jpg',
+        profession: 'Developer',
+      },
+      show: false,
+      elapsedTime: 0,
+    };
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState((prevState) => ({
+        elapsedTime: prevState.elapsedTime + 1,
+      }));
+    }, 1000); // Met à jour l'intervalle chaque seconde
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  toggleShow = () => {
+    this.setState((prevState) => ({
+      show: !prevState.show,
+    }));
+  };
+
+  render() {
+    const { person, show, elapsedTime } = this.state;
+
+    return (
+      <div style={{ alignItems: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'column' }}>
+        <h1 style={{color: 'black'}}>Clique sur le bouton pour voir le bg</h1>
+        <button style={{color: 'whitesmoke',backgroundColor: 'black', hover: 'true', cursor: 'pointer', padding: '10px', borderRadius: '10px', boxShadow: '3px 5px 5px gold', fontSize: '18px', fontFamily: 'cursive' }} onClick={this.toggleShow}>Voir le BG</button>
+        
+        {show && (
+          <div style={{ backgroundColor: 'black', boxShadow: '10px 10px 25px gold', color: 'white', padding: '20px', width: '200px', borderRadius: '20px', position:'relative', top: '40px' }}>
+            <h2 style={{fontFamily: 'serif',}}>Name: {person.fullName}</h2>
+            <p style={{fontFamily: 'cursive'}}>Bio: {person.bio}</p>
+            <img src={person.imgSrc} style={{ width: '150px', height: '150px', borderRadius: '50%' }} alt="Person Avatar" />
+            <p style={{fontFamily: 'cursive'}}>Profession: {person.profession}</p>
+          </div>
+        )}
+
+        <p style={{position:'relative', top: '50px', color: 'black', fontFamily: 'cursive'}}>Elapsed Time (tkt, c'est normal que tu sois rester si longtemps): {elapsedTime} seconds</p>
+      </div>
+    );
+  }
 }
 
 export default App;
